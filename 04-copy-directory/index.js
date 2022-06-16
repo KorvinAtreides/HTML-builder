06041ed2ce055b9df4dd0da7cbfbe988e88ca489
KorvinAtreides <Korvin1312b@yandex.by>
2022-06-16 14:50:12 +0300

    feat: add 04-copy-directory script

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
 
+const readdir = fs.promises.readdir;
+const mkdir = fs.promises.mkdir;
+const copyFile = fs.promises.copyFile;
+const unlink = fs.promises.unlink;
+
+mkdir(path.join(__dirname, 'files-copy'), {"recursive": true} );
+const oldFiles = readdir(path.join(__dirname, './files-copy'), { "withFileTypes": true });
+oldFiles.then(filesArray => {
+  for (const file of filesArray) unlink(path.join(__dirname, './files-copy', file.name))
+});
+
+const files = readdir(path.join(__dirname, 'files'), { "withFileTypes": true });
+files.then(filesArray => {
+  for (const file of filesArray){
+    copyFile(
+      path.join(__dirname, 'files', file.name),
+      path.join(__dirname, 'files-copy', file.name),
+      fs.constants.COPYFILE_FICLONE,
+    );
+  }
+});
